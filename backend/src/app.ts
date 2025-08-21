84e061a18880afd82ebb1c7426fd0e3a83dae91d
Douglas Welber <douglasweelber.2004@outlook.com>
2025-08-21 20:36:11 -0300

    chore: [BACKEND] create base project

---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+export const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
